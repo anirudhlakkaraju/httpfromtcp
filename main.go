e10e310e52209609954f6686b791a17f4475437a
anirudhlakkaraju <lakanirudh123@gmail.com>
2026-01-08 21:50:39 -0600

    print data by line in file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -13,6 +14,7 @@ func main() {
 		log.Fatal("error", "error", err)
 	}
 
+	str := ""
 	for {
 		data := make([]byte, 8)
 		n, err := f.Read(data)
@@ -20,6 +22,25 @@ func main() {
 			break
 		}
 
-		fmt.Printf("read: %s\n", string(data[:n]))
+		data = data[:n]
+
+		// If new line present in 8 byte window
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+
+			// Append data until the new line
+			str += string(data[:i])
+
+			// Move window to data after new line
+			data = data[i+1:]
+
+			// Display str so far
+			fmt.Printf("read: %s\n", str)
+
+			// Reset str for next line
+			str = ""
+		}
+
+		// Append data to str - either the entire window or the part after new line
+		str += string(data)
 	}
 }
