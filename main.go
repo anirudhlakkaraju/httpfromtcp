0f82ffffdb2385da1d77140f64545c766b875f83
anirudhlakkaraju <lakanirudh123@gmail.com>
2026-01-12 18:55:33 -0600

    read lines and send to channel

--- main.go
+++ main.go
@@ -3,9 +3,52 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	str := ""
+	go func() {
+		defer f.Close()
+		defer close(out)
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+
+			// If new line present in 8 byte window
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+
+				// Append data until the new line
+				str += string(data[:i])
+
+				// Move window to data after new line
+				data = data[i+1:]
+
+				out <- str
+
+				// Reset str for next line
+				str = ""
+			}
+
+			// Append data to str - either the entire window or the part after new line
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
 
 func main() {
 
@@ -14,33 +57,8 @@ func main() {
 		log.Fatal("error", "error", err)
 	}
 
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-
-		data = data[:n]
-
-		// If new line present in 8 byte window
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-
-			// Append data until the new line
-			str += string(data[:i])
-
-			// Move window to data after new line
-			data = data[i+1:]
-
-			// Display str so far
-			fmt.Printf("read: %s\n", str)
-
-			// Reset str for next line
-			str = ""
-		}
-
-		// Append data to str - either the entire window or the part after new line
-		str += string(data)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
